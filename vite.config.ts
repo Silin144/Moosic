e45e3eb7d7ef1ceca1a1e73d2948b2c145f6a0cf
Silin144 <sr4gupta@uwaterloo.ca>
2025-04-08 09:09:04 -0400

    Update Vercel build configuration and scripts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: 'https://moosic-liart.vercel.app/',
+  base: '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
